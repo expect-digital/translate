758217ad8154b2920244bd870dfc5c2b97f043df
Daria Leonova <darja.leonova.it@gmail.com>
2022-12-28 16:01:26 +0200

    Added changes in code

--- pkg/convert/go_messages.go
+++ pkg/convert/go_messages.go
@@ -9,34 +9,31 @@ import (
 )
 
 func FromGoMessages(m model.Messages) ([]byte, error) {
-	pipelineMsg := MessagesToPipeline(m)
+	pipelineMsg := messagesToPipeline(m)
 
 	msg, err := json.Marshal(pipelineMsg)
 	if err != nil {
-		return []byte{}, fmt.Errorf("error while marshaling: %w", err)
+		return nil, fmt.Errorf("error while marshaling: %w", err)
 	}
 
 	return msg, nil
 }
 
-func ToGoMessages(b []byte) (pipeline.Messages, error) {
-	var msg pipeline.Messages
+func ToGoMessages(b []byte) (model.Messages, error) {
+	var msg model.Messages
 
 	err := json.Unmarshal(b, &msg)
 	if err != nil {
-		return pipeline.Messages{}, fmt.Errorf("error while unmarshaling: %w", err)
+		return model.Messages{}, fmt.Errorf("error while unmarshaling: %w", err)
 	}
 
 	return msg, nil
 }
 
-func MessagesToPipeline(m model.Messages) pipeline.Messages {
-	pipelineMsg := pipeline.Messages{}
-
-	pipelineMsg.Language = m.Language
-
-	for key, value := range m.Labels {
-		pipelineMsg.Macros[key] = pipeline.Text{Msg: value}
+func messagesToPipeline(m model.Messages) pipeline.Messages {
+	pipelineMsg := pipeline.Messages{
+		Language: m.Language,
+		Messages: make([]pipeline.Message, 0, len(m.Messages)),
 	}
 
 	for _, value := range m.Messages {
