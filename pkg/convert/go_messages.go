e9bf1bbf5b910ae4311a826f97e09af1d8ce463a
Daria Leonova <darja.leonova.it@gmail.com>
2022-12-28 13:39:55 +0200

    Added conversion Messages model to pipeline.Messages

--- pkg/convert/go_messages.go
+++ pkg/convert/go_messages.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 
 	"github.com/expect-digital/translate/pkg/model"
+	"golang.org/x/text/message/pipeline"
 )
 
 func FromGoMessages(m model.Messages) ([]byte, error) {
-	msg, err := json.Marshal(m)
+	pipelineMsg := MessagesToPipeline(m)
+
+	msg, err := json.Marshal(pipelineMsg)
 	if err != nil {
 		return []byte{}, fmt.Errorf("error while marshaling: %w", err)
 	}
@@ -16,13 +19,33 @@ func FromGoMessages(m model.Messages) ([]byte, error) {
 	return msg, nil
 }
 
-func ToGoMessages(b []byte) (model.Messages, error) {
-	var msg model.Messages
+func ToGoMessages(b []byte) (pipeline.Messages, error) {
+	var msg pipeline.Messages
 
 	err := json.Unmarshal(b, &msg)
 	if err != nil {
-		return model.Messages{}, fmt.Errorf("error while unmarshaling: %w", err)
+		return pipeline.Messages{}, fmt.Errorf("error while unmarshaling: %w", err)
 	}
 
 	return msg, nil
+}
+
+func MessagesToPipeline(m model.Messages) pipeline.Messages {
+	pipelineMsg := pipeline.Messages{}
+
+	pipelineMsg.Language = m.Language
+
+	for key, value := range m.Labels {
+		pipelineMsg.Macros[key] = pipeline.Text{Msg: value}
+	}
+
+	for _, value := range m.Messages {
+		pipelineMsg.Messages = append(pipelineMsg.Messages, pipeline.Message{
+			ID:      append(pipeline.IDList{}, value.ID),
+			Fuzzy:   value.Fuzzy,
+			Message: pipeline.Text{Msg: value.Message},
+		})
+	}
+
+	return pipelineMsg
 }
