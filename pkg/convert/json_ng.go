d9af3b4dc07afedc55aa90de304ade68e46408d0
Daria Leonova <darja.leonova.it@gmail.com>
2022-12-30 16:46:43 +0200

    #13/Add-Ng-JSON-conversation Added NgJson struct and adjusted functions

--- pkg/convert/json_ng.go
+++ pkg/convert/json_ng.go
@@ -8,54 +8,48 @@ import (
 	"golang.org/x/text/language"
 )
 
+type NgJson struct {
+	Locale       language.Tag      `json:"locale"`
+	Translations map[string]string `json:"translations"`
+}
+
 func FromNgJson(b []byte) (model.Messages, error) {
-	incoming := make(map[string]interface{})
+	var ngJson NgJson
 
-	err := json.Unmarshal(b, &incoming)
+	err := json.Unmarshal(b, &ngJson)
 	if err != nil {
-		return model.Messages{}, fmt.Errorf("decode NG JSON to go map: %w", err)
-	}
-
-	tag, err := language.Parse(fmt.Sprint(incoming["locale"]))
-	if err != nil {
-		return model.Messages{}, fmt.Errorf("parse NG JSON key: locale : %w", err)
+		return model.Messages{}, fmt.Errorf("decode NG JSON to go messages: %w", err)
 	}
 
 	msg := model.Messages{
-		Language: tag,
+		Language: ngJson.Locale,
 		Messages: []model.Message{},
 	}
 
-	if translations, isMap := incoming["translations"].(map[string]interface{}); isMap {
-		for key, value := range translations {
-			if message, isString := value.(string); isString {
-				msg.Messages = append(msg.Messages, model.Message{ID: key, Message: message})
-			} else {
-				return model.Messages{}, fmt.Errorf("type check of translation with ID: %s value : %w", key, err)
-			}
-		}
-	} else {
-		return model.Messages{}, fmt.Errorf("type check of translations map: %w", err)
+	for key, value := range ngJson.Translations {
+		msg.Messages = append(msg.Messages, model.Message{
+			ID:      key,
+			Message: value,
+		})
 	}
 
 	return msg, nil
 }
 
 func ToNgJson(m model.Messages) ([]byte, error) {
-	messages := make(map[string]interface{})
-	result := make(map[string]interface{})
+	ngJson := NgJson{
+		Locale:       m.Language,
+		Translations: make(map[string]string),
+	}
 
 	for _, message := range m.Messages {
-		messages[message.ID] = message.Message
+		ngJson.Translations[message.ID] = message.Message
 	}
 
-	result["locale"] = m.Language.String()
-	result["translations"] = messages
-
-	msg, err := json.Marshal(result)
+	result, err := json.Marshal(ngJson)
 	if err != nil {
-		return nil, fmt.Errorf("encode model.Messages to NG JSON: %w", err)
+		return nil, fmt.Errorf("encode go NgJson to []byte: %w", err)
 	}
 
-	return msg, nil
+	return result, nil
 }
