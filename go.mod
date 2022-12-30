add0f65f4d2847feedb48370a2d1366c3694861f
DarjaLeonova <94634373+DarjaLeonova@users.noreply.github.com>
2022-12-30 15:22:40 +0200

    #15/Add go conversation to pkg/convert/go_messages.go (#18)
    * #15/Add go conversation to pkg/convert/go_messages.go
    
    * Added fixes
    
    * Added fixes
    
    * Renamed functions
    
    * Added conversion Messages model to pipeline.Messages
    
    * Added changes in code
    
    * Added messagesFromPipeline function
    
    * changed error format
    
    * changed error format again
    
    * Added fixes
    
    * #15/Add-go-conversation

--- go.mod
+++ go.mod
@@ -17,7 +17,9 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/convert/go_messages.go
@@ -0,0 +1,67 @@
+package convert
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/expect-digital/translate/pkg/model"
+	"golang.org/x/text/message/pipeline"
+)
+
+func FromGoMessages(m model.Messages) ([]byte, error) {
+	pipelineMsg := messagesToPipeline(m)
+
+	msg, err := json.Marshal(pipelineMsg)
+	if err != nil {
+		return nil, fmt.Errorf("encode pipeline.Messages to JSON: %w", err)
+	}
+
+	return msg, nil
+}
+
+func ToGoMessages(b []byte) (model.Messages, error) {
+	var pipelineMsg pipeline.Messages
+
+	err := json.Unmarshal(b, &pipelineMsg)
+	if err != nil {
+		return model.Messages{}, fmt.Errorf("decode JSON to pipeline.Messages: %w", err)
+	}
+
+	msg := messagesFromPipeline(pipelineMsg)
+
+	return msg, nil
+}
+
+func messagesToPipeline(m model.Messages) pipeline.Messages {
+	pipelineMsg := pipeline.Messages{
+		Language: m.Language,
+		Messages: make([]pipeline.Message, 0, len(m.Messages)),
+	}
+
+	for _, value := range m.Messages {
+		pipelineMsg.Messages = append(pipelineMsg.Messages, pipeline.Message{
+			ID:      pipeline.IDList{value.ID},
+			Fuzzy:   value.Fuzzy,
+			Message: pipeline.Text{Msg: value.Message},
+		})
+	}
+
+	return pipelineMsg
+}
+
+func messagesFromPipeline(m pipeline.Messages) model.Messages {
+	msg := model.Messages{
+		Language: m.Language,
+		Messages: make([]model.Message, 0, len(m.Messages)),
+	}
+
+	for _, value := range m.Messages {
+		msg.Messages = append(msg.Messages, model.Message{
+			ID:      value.ID[0],
+			Fuzzy:   value.Fuzzy,
+			Message: value.Message.Msg,
+		})
+	}
+
+	return msg
+}
