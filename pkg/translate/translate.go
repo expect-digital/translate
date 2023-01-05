d7eb779c3fe151dd282cd48a9b9b0bd3b030bc59
Vladislavs <vladislavs.perkanuks@expect.digital>
2023-01-05 10:55:06 +0200

    #22 Fixes

--- pkg/translate/translate.go
+++ pkg/translate/translate.go
@@ -2,11 +2,10 @@ package translate
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/expect-digital/translate/pkg/convert"
-	"github.com/expect-digital/translate/pkg/model"
 	pb "github.com/expect-digital/translate/pkg/server/translate/v1"
+	"golang.org/x/text/language"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -23,34 +22,48 @@ func (t *TranslateServiceServer) UploadTranslationFile(
 	ctx context.Context,
 	req *pb.UploadTranslationFileRequest,
 ) (*pb.UploadTranslationFileResponse, error) {
-	if len(req.GetTranslationId()) == 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "Missing translation_id")
-	}
-
 	var (
-		messages model.Messages
-		err      error
+		reqTranslationID = req.GetTranslationId()
+		reqData          = req.GetData()
+		reqLabels        = req.GetLabels()
+		reqType          = req.GetType()
+		reqLanguage      = req.GetLanguage()
 	)
 
-	switch req.GetType() {
-	default:
-		return nil, status.Errorf(codes.Internal, "")
-	case pb.Type_TYPE_UNSPECIFIED:
-		return nil, status.Errorf(codes.InvalidArgument, "Type is missing")
-	case pb.Type_NG_LOCALISE:
-		messages, err = convert.FromNgJson([]byte(req.Data))
-	case pb.Type_NGX_TRANSLATE:
-		messages, err = convert.FromNgxTranslate([]byte(req.Data))
-	case pb.Type_GO:
-		messages, err = convert.FromGo([]byte(req.Data))
+	if len(reqTranslationID) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "missing translation_id")
 	}
 
-	// todo Proper error handling with status codes
+	if len(reqLanguage) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "language is missing")
+	}
+
+	language, err := language.Parse(reqLanguage)
 	if err != nil {
-		return nil, fmt.Errorf("convert to messages: %w", err)
+		return nil, status.Errorf(codes.InvalidArgument, "parse language: %s", err)
+	}
+
+	var from convert.From
+
+	switch reqType {
+	case pb.Type_TYPE_UNSPECIFIED:
+		return nil, status.Errorf(codes.InvalidArgument, "type is missing")
+	case pb.Type_NG_LOCALISE:
+		from = convert.FromNgJson
+	case pb.Type_NGX_TRANSLATE:
+		from = convert.FromNgxTranslate
+	case pb.Type_GO:
+		from = convert.FromGo
+	}
+
+	messages, err := from([]byte(reqData))
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "parse data: %s", err)
 	}
 
 	// Save to DB/FS...
+	messages.Language = language
+	messages.Labels = reqLabels
 	_ = messages
 
 	return &pb.UploadTranslationFileResponse{}, nil
