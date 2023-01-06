8ed8eadc51e0c027e41dd6600b4f698d4a36de10
DarjaLeonova <94634373+DarjaLeonova@users.noreply.github.com>
2023-01-06 17:45:14 +0200

    #26/ Store messages in in-memory BadgerDB (#37)
    * #26/ Store messages in in-memory BadgerDB
    
    * #26/ Store messages in in-memory BadgerDB
    
    Added fixes
    
    * #26/ Store messages in in-memory BadgerDB
    
    Added repo folder to pkg folder
    
    * #26/ Store messages in in-memory BadgerDB
    
    Added error check in main.go & deleted comment in translate.go
    
    * #26/ Store messages in in-memory BadgerDB
    
    deleted encodeMessages() in repo.go
    
    * #26/ Store messages in in-memory BadgerDB
    
    * #26/ Store messages in in-memory BadgerDB

---
+++ pkg/repo/repo.go
@@ -0,0 +1,48 @@
+package repo
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dgraph-io/badger/v3"
+	"github.com/expect-digital/translate/pkg/model"
+)
+
+type Repo struct {
+	db *badger.DB
+}
+
+func NewRepo(db *badger.DB) *Repo {
+	return &Repo{
+		db: db,
+	}
+}
+
+func Connect() (*badger.DB, error) {
+	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	if err != nil {
+		return nil, fmt.Errorf("connect to DB: %w", err)
+	}
+
+	return db, nil
+}
+
+func (repo *Repo) SaveMessages(id string, m model.Messages) error {
+	messagesJson, err := json.Marshal(m)
+	if err != nil {
+		return fmt.Errorf("marshaling model.Messages to JSON: %w", err)
+	}
+
+	err = repo.db.Update(func(txn *badger.Txn) error {
+		err = txn.Set([]byte(id), messagesJson)
+		if err != nil {
+			err = fmt.Errorf("setting key/value pairs %w", err)
+		}
+		return err
+	})
+	if err != nil {
+		return fmt.Errorf("creating read/write transaction: %w", err)
+	}
+
+	return nil
+}
