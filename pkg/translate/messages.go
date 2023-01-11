12d46545e45f58c07a15ee8b807b843633fcbdb3
Vladislavs <vladislavs.perkanuks@responsivecraft.com>
2023-01-11 12:14:03 +0200

    #29 Update messages implementation

--- pkg/translate/messages.go
+++ pkg/translate/messages.go
@@ -2,9 +2,13 @@ package translate
 
 import (
 	"context"
+	"errors"
 
+	"github.com/expect-digital/translate/pkg/model"
+	"github.com/expect-digital/translate/pkg/repo"
 	pb "github.com/expect-digital/translate/pkg/server/translate/v1"
 	"github.com/expect-digital/translate/pkg/transform"
+	"golang.org/x/exp/slices"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,24 +18,40 @@ func (t *TranslateServiceServer) UpdateMessages(
 	req *pb.UpdateMessagesRequest,
 ) (*pb.UpdateMessagesResponse, error) {
 	var (
-		reqMessagesID = req.GetMessagesId()
-		reqMessages   = req.GetMessages()
+		reqTranslationID = req.GetTranslationId()
+		reqMessages      = req.GetMessages()
 	)
 
-	if len(reqMessagesID) == 0 {
+	if len(reqTranslationID) == 0 {
 		return nil, status.Errorf(codes.InvalidArgument, "missing translation_id")
 	}
 
+	reqMessages.TranslationId = reqTranslationID
+
 	messages, err := transform.MessagesFromProto(reqMessages)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "parse messages: %s", err)
+		return nil, status.Errorf(codes.InvalidArgument, "parsing messages: %s", err)
 	}
 
-	// Load from DB
-	// Update with new values
-	// Store updated
+	messagesList, err := t.repo.LoadMessages(reqTranslationID)
 
-	_ = messages
+	switch {
+	case errors.Is(err, repo.ErrNotFound):
+		return nil, status.Errorf(codes.NotFound, "no translations with id '%s'", reqTranslationID)
+	case err != nil:
+		return nil, status.Errorf(codes.Internal, "loading messages: %s", err)
+	}
+
+	if cnt := slices.ContainsFunc(messagesList, func(m model.Messages) bool {
+		return m.Language.String() == messages.Language.String()
+	}); !cnt {
+		return nil, status.Errorf(codes.NotFound, "no such language exists in Database '%s'", messages.Language.String())
+	}
+
+	err = t.repo.SaveMessages(messages)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "saving messages: %s", err)
+	}
 
 	return &pb.UpdateMessagesResponse{}, nil
 }
@@ -42,7 +62,7 @@ func (t *TranslateServiceServer) ListMessages(
 ) (*pb.ListMessagesResponse, error) {
 	allMessages, err := t.repo.ListMessages()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "list all messages: %s", err)
+		return nil, status.Errorf(codes.Internal, "listing all messages: %s", err)
 	}
 
 	allMessagesPb := make([]*pb.Messages, 0, len(allMessages))
